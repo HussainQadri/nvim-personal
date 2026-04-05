5261fcea77e61564887d0f8f30151daf4150f83c
HussainQadri <hussain.bqadri@gmail.com>
2026-04-05 01:38:55 +0100

    Diagnostics

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -39,6 +39,25 @@ return {
 
       vim.lsp.enable({ "clangd", "ty" })
 
+      vim.diagnostic.config({
+        underline = true,
+        update_in_insert = false,
+        virtual_text = {
+          spacing = 4,
+          source = "if_many",
+          prefix = "●",
+        },
+        severity_sort = true,
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = " ",
+            [vim.diagnostic.severity.WARN] = " ",
+            [vim.diagnostic.severity.HINT] = " ",
+            [vim.diagnostic.severity.INFO] = " ",
+          },
+        },
+      })
+
       vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(args)
           local client = vim.lsp.get_client_by_id(args.data.client_id)
