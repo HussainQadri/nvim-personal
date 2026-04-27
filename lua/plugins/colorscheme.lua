fafaea9f2b24d4dfe0af65e1477cbf2c296a61bc
Hussain Qadri <hussain.bqadri@gmail.com>
2026-04-27 22:03:31 +0100

    Stick to koda theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,24 +1,26 @@
 return {
-  "oskarnurm/koda.nvim",
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    local soft = "#cfcfcf"
-    local mid  = "#a8a8a8"
-    require("koda").setup({
-      transparent = true,
-      colors = {
-        emphasis = soft,
-        func     = soft,
-        string   = soft,
-        char     = soft,
-        special  = soft,
-        border   = soft,
-        keyword  = mid,
-        type     = mid,
-        operator = mid,
-      },
-    })
-    vim.cmd("colorscheme koda-dark")
-  end,
+  {
+    "oskarnurm/koda.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      local soft = "#cfcfcf"
+      local mid  = "#a8a8a8"
+      require("koda").setup({
+        transparent = true,
+        colors = {
+          emphasis = soft,
+          func     = soft,
+          string   = soft,
+          char     = soft,
+          special  = soft,
+          border   = soft,
+          keyword  = mid,
+          type     = mid,
+          operator = mid,
+        },
+      })
+      vim.cmd.colorscheme("koda-dark")
+    end,
+  },
 }
