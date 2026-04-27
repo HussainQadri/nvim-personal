75b8ee4d0d8d4371ac04bf7a3565b2fad56fde22
Hussain Qadri <hussain.bqadri@gmail.com>
2026-04-27 22:03:38 +0100

    Add lua formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,26 +1,34 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    event = "BufWritePre",
-    cmd = "ConformInfo",
-    opts = {
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_format = "fallback",
-      },
-      formatters_by_ft = {
-        c = { "clang-format" },
-        cpp = { "clang-format" },
-        h = { "clang-format" },
-        hpp = { "clang-format" },
-      },
-      formatters = {
-        ["clang-format"] = {
-          prepend_args = {
-            '--style={BasedOnStyle: LLVM, IndentWidth: 4, AccessModifierOffset: -4, AllowShortFunctionsOnASingleLine: None, PointerAlignment: Left, ReferenceAlignment: Left}',
-          },
+    {
+        "stevearc/conform.nvim",
+        event = "BufWritePre",
+        cmd = "ConformInfo",
+        opts = {
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_format = "fallback",
+            },
+            formatters_by_ft = {
+                c = { "clang-format" },
+                cpp = { "clang-format" },
+                h = { "clang-format" },
+                hpp = { "clang-format" },
+
+                javascript = { "prettier" },
+                javascriptreact = { "prettier" },
+                typescript = { "prettier" },
+                typescriptreact = { "prettier" },
+                css = { "prettier" },
+                html = { "prettier" },
+                json = { "prettier" },
+            },
+            formatters = {
+                ["clang-format"] = {
+                    prepend_args = {
+                        "--style={BasedOnStyle: LLVM, IndentWidth: 4, AccessModifierOffset: -4, AllowShortFunctionsOnASingleLine: None, PointerAlignment: Left, ReferenceAlignment: Left}",
+                    },
+                },
+            },
         },
-      },
     },
-  },
 }
