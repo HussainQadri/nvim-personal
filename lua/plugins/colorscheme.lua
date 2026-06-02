d83d0f90714d14cf9b1d8a837592d05b70ef93c8
Hussain Qadri <hussain.bqadri@gmail.com>
2026-06-02 21:09:26 +0100

    strings are green

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,26 +1,27 @@
 return {
-  {
-    "oskarnurm/koda.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      local soft = "#cfcfcf"
-      local mid  = "#a8a8a8"
-      require("koda").setup({
-        transparent = true,
-        colors = {
-          emphasis = soft,
-          func     = soft,
-          string   = soft,
-          char     = soft,
-          special  = soft,
-          border   = soft,
-          keyword  = mid,
-          type     = mid,
-          operator = mid,
-        },
-      })
-      vim.cmd.colorscheme("koda-dark")
-    end,
-  },
+    {
+        "oskarnurm/koda.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            local soft  = "#cfcfcf"
+            local green = "#98c379"
+            local mid   = "#a8a8a8"
+            require("koda").setup({
+                transparent = true,
+                colors = {
+                    emphasis = soft,
+                    func     = soft,
+                    string   = green,
+                    char     = green,
+                    special  = soft,
+                    border   = soft,
+                    keyword  = mid,
+                    type     = mid,
+                    operator = mid,
+                },
+            })
+            vim.cmd.colorscheme("koda-dark")
+        end,
+    },
 }
