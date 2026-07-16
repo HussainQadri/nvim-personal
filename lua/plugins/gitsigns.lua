6346b09f5a5f54be9653d5ef1f9f1702ab220038
Hussain Qadri <hussain.bqadri@gmail.com>
2026-07-16 13:36:08 +0100

    Navigate hunks with ]h and [h

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,6 +3,17 @@ return {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
     opts = {
+      on_attach = function(bufnr)
+        local gitsigns = require("gitsigns")
+        local function map(lhs, direction, desc)
+          vim.keymap.set("n", lhs, function()
+            gitsigns.nav_hunk(direction)
+          end, { buffer = bufnr, desc = desc })
+        end
+
+        map("]h", "next", "Next Git Hunk")
+        map("[h", "prev", "Previous Git Hunk")
+      end,
       signs = {
         add = { text = "▎" },
         change = { text = "▎" },
